2a3792a915ab3cf4ea15ecbffb01c7633cd92d28
sooriya <sooriya@pyps.in>
2022-10-19 13:04:24 +0530

    changes on test

---
+++ kodo-exercise/index.test.js
@@ -0,0 +1,19 @@
+// const { response } = require('express');
+// const request = require('supertest')
+// const app = require('./index')
+// describe('API',()=>{
+//     it('POST /list --> search', ()=>{
+//         return request(app).post('/list').expect(200)
+// .then((response)=>{
+//     expect(response.body).toEqual(
+//         expect.objectContaining({
+//             name:expect.any(String)
+//         })
+//     )
+// })
+//     });
+// })
+
+// // it('should run',()=>{
+    
+// // })
